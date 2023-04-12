ec8488d90c04e60ba21b19e9019f6459170f287c
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-04-13 03:01:58 +0900

    add:favorite_reviewモデルの追加

--- backend/app/models/review.rb
+++ backend/app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
 
   belongs_to :user
   belongs_to :book
+  has_many :favorite_reviews, dependent: :destroy
+
 
   validates :rating,   presence: true
   validates :title,    presence: true, length: { maximum: 60 }
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :replies
   has_many :subject_question_replies
   has_many :favorite_books, dependent: :destroy
+  has_many :favorite_reviews, dependent: :destroy
   has_many :favorite_questions, dependent: :destroy
 
 
