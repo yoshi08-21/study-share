01b33ba26c9384f5716c4f9e646722a501fc3fe2
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-04-20 12:40:23 +0900

    feat:参考書の詳細ページでお気に入りの有無に応じてお気に入りボタンを切り替える機能を実装

--- backend/app/controllers/books_controller.rb
+++ backend/app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  include RecordChecker
 
   def index
     books = Book.all
@@ -6,6 +7,26 @@ class BooksController < ApplicationController
       render json: books
     else
       render json: books.errors
+    end
+  end
+
+  def show
+    book = Book.find_by(id: params[:id])
+    if book
+      render json: book
+    else
+      render json:book.errors
+    end
+  end
+
+  def is_favorite
+    current_user = User.find_by(id: params[:user_id])
+    book = Book.find_by(id: params[:book_id])
+    favorite_book = FavoriteBook.find_by(user_id: current_user.id, book_id: book.id)
+    if favorite_book
+      render json: { is_favorite: true, favorite_book_id: favorite_book.id }
+    else
+      render json: false
     end
   end
 
---
+++ backend/db/migrate/20230420033900_add_index_to_favorite_books.rb
@@ -0,0 +1,4 @@
+class AddIndexToFavoriteBooks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
