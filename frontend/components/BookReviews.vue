b079b6b73fa22a376e969f7be0aec81a4ca5a87a
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-04-27 13:22:43 +0900

    feat:参考書に紐づくレビュ一一覧の表示機能を追加

--- frontend/components/BookReviews.vue
+++ frontend/components/BookReviews.vue
@@ -2,12 +2,22 @@
   <div>
     <h3>レビュー一覧</h3>
 
+    <v-flex xs12 sm6 md4 lg3 mb-5 v-for="(review, index) in reviews" :key="index">
+
+    <v-card >
+      <v-card-title>{{ review.title }} 評価：{{ review.rating }}</v-card-title>
+      <v-card-text>{{ review.content }}</v-card-text>
+    </v-card>
+
+    </v-flex>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    reviews: []
+  }
 }
 </script>
 
