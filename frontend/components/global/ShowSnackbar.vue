7244d7ac9101ab3ffef12bcf0ed7f83c53c4a9b5
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-05-04 09:02:23 +0900

    feat:レビュー削除時の確認ダイアログを実装

---
+++ frontend/components/global/ShowSnackbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-snackbar v-model="snackbar" :color="color">{{ message }}</v-snackbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      snackbar: {
+        show: false,
+        message: "",
+        timeout: 3000,
+        color: ""
+      }
+    }
+  },
+  methods: {
+    showSnackbar(message, color) {
+      this.snackbar.show = true
+      this.snackbar.message = message
+      this.snackbar.color = color
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
