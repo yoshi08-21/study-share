7bb55deaae90581694b66fb1fa07c5d1157679ad
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-05-18 07:40:29 +0900

    不要なファイルを削除

--- frontend/.nuxt/middleware.js
+++ frontend/.nuxt/middleware.js
@@ -3,15 +3,6 @@ const middleware = {}
 middleware['authCheck'] = require('../middleware/authCheck.js')
 middleware['authCheck'] = middleware['authCheck'].default || middleware['authCheck']
 
-middleware['authenticated'] = require('../middleware/authenticated.js')
-middleware['authenticated'] = middleware['authenticated'].default || middleware['authenticated']
-
-middleware['currentUser'] = require('../middleware/currentUser.js')
-middleware['currentUser'] = middleware['currentUser'].default || middleware['currentUser']
-
-middleware['getUser'] = require('../middleware/getUser.js')
-middleware['getUser'] = middleware['getUser'].default || middleware['getUser']
-
 middleware['isLoggedIn'] = require('../middleware/isLoggedIn.js')
 middleware['isLoggedIn'] = middleware['isLoggedIn'].default || middleware['isLoggedIn']
 
