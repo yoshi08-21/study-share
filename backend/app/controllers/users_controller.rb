184a749f22a76d2df11d51308a940dd2db133170
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-05-19 05:13:35 +0900

    fix:レビューの編集が失敗したときに、成功のフラッシュメッセージが出てしまうバグを修正

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -31,18 +31,18 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user
+      render json: user, status: 200
     else
-      render json: user.errors
+      render json: user.errors, status: 400
     end
   end
 
   def update
     user = User.find_by(id: params[:id])
     if user.update(user_params)
-      render json: user
+      render json: user, status: 200
     else
-      render json: user.errors
+      render json: user.errors, status: 400
     end
   end
 
