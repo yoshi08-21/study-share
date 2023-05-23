fe5e6c6f6a3ed0a78e217f8f44d5b988c1cf3713
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-05-23 16:22:53 +0900

    commit

--- frontend/.nuxt/components/index.js
+++ frontend/.nuxt/components/index.js
@@ -1,6 +1,4 @@
-export { default as NuxtLogo } from '../../components/NuxtLogo.vue'
-export { default as Tutorial } from '../../components/Tutorial.vue'
-export { default as VuetifyLogo } from '../../components/VuetifyLogo.vue'
+export { default as ShowSnackbar } from '../../components/global/ShowSnackbar.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
