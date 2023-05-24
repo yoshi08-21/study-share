ef0d3901ca1be16fffeebcbd8eb91ec623b21b70
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-05-24 18:53:35 +0900

    feat:いいね！した返信一覧をお気に入りページに追加

--- backend/app/controllers/favorites_controller.rb
+++ backend/app/controllers/favorites_controller.rb
@@ -5,11 +5,13 @@ class FavoritesController < ApplicationController
     favorite_books = current_user.fav_books
     favorite_reviews = current_user.fav_reviews.includes(:user, :book)
     favorite_questions = current_user.fav_questions.includes(:user, :book)
+    favorite_replies = current_user.fav_replies.includes(:user, :question)
     if current_user
       render json: {
         favorite_books: favorite_books,
         favorite_reviews: favorite_reviews.as_json(include: [:user, :book]),
-        favorite_questions: favorite_questions.as_json(include: [:user, :book])
+        favorite_questions: favorite_questions.as_json(include: [:user, :book]),
+        favorite_replies: favorite_replies.as_json(include: [:user, :question])
       },
       status: 200
     else
