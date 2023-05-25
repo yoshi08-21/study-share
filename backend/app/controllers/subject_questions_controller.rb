95861662c94666b716e8a1327875fc534d8335b3
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-05-25 16:57:23 +0900

    feat:科目別質問機能を実装

--- backend/app/controllers/subject_questions_controller.rb
+++ backend/app/controllers/subject_questions_controller.rb
@@ -1,11 +1,98 @@
 class SubjectQuestionsController < ApplicationController
 
   def index
-    subject_questions = SubjectQuestion.all
+    subject_questions = SubjectQuestion.includes(:user).all
     if subject_questions
-      render json: subject_questions
+      render json: subject_questions, include: "user"
     else
       render json: subject_questions.errors
     end
   end
+
+
+  def show
+    subject_question = SubjectQuestion.includes(:user).find_by(id: params[:id])
+    # favorite_questions = FavoriteQuestion.where(question_id: question.id)
+    # favorite_questions_count = favorite_questions.count
+    if subject_question
+      render json: {
+        subject_question: subject_question.as_json(include: :user),
+        # favorite_subject_questions_count: favorite_subject_questions_count
+      }
+    else
+      render json: subject_question.errors
+    end
+  end
+
+
+  def create
+    current_user = User.find_by(id: params[:user_id])
+    subject_question = current_user.subject_questions.build(subject_question_params)
+    if subject_question.save
+      render json: subject_question, status: 200
+    else
+      render json: { error: "エラーが発生しました" }, status: 400
+    end
+  end
+
+  def update
+    current_user = User.find_by(id: params[:current_user_id])
+    subject_question = SubjectQuestion.find_by(id: params[:id])
+    author = subject_question.user
+    if validate_authorship(current_user, author)
+      if subject_question.update(subject_question_params)
+        render json: subject_question, status: 200
+      else
+        render json: { error: "エラーが発生しました" }, status: 400
+      end
+    else
+      render json: { error: "権限がありません" }, status: 400
+    end
+  end
+
+  def destroy
+    current_user = User.find_by(id: params[:current_user_id])
+    subject_question = SubjectQuestion.find_by(id: params[:id])
+    author = subject_question.user
+    if validate_authorship(current_user, author)
+      if subject_question.destroy
+        head :no_content
+      else
+        render json: { error: "エラーが発生しました" }, status: 400
+      end
+    else
+      render json: { error: "権限がありません" }, status: 400
+    end
+  end
+
+  def is_favorite
+    current_user = User.find_by(id: params[:user_id])
+    question = Question.find_by(id: params[:question_id])
+    favorite_question = FavoriteQuestion.find_by(user_id: current_user.id, question_id: question.id)
+    if favorite_question
+      render json: { is_favorite: true, favorite_question_id: favorite_question.id }
+    else
+      render json: false
+    end
+  end
+
+  def all_questions
+    questions = Question.includes(:user, :book).all
+    if questions
+      render json: questions, include: [:user, :book]
+    else
+      render jso: questions.errors
+    end
+  end
+
+
+  private
+
+    def subject_question_params
+      params.require(:subject_question).permit(:title, :content, :subject, :user_id)
+    end
+
+
+
+
 end
--- frontend/pages/subjectQuestions/subjectQuestions.vue
+++ frontend/pages/subjectQuestions/subjectQuestions.vue
@@ -21,41 +21,96 @@
         <v-text-field></v-text-field><v-btn>検索</v-btn>
       </v-card-title>
       <v-card-title>
-        <v-btn block>質問を投稿する</v-btn>
+        <v-btn @click="dialog = true" block>科目別質問を投稿する</v-btn>
       </v-card-title>
     </v-card>
 
-    <br>
-    <v-flex xs12 sm6 md4 lg3 mb-5 v-for="(question, index) in questions" :key="index">
-      <v-card :to="`/books/${question.book_id}/questions/${question.id}`">
-        <v-card-title>{{ question.title }}</v-card-title>
-        <v-card-text>{{ question.content }}</v-card-text>
-        <template v-if="question.user">
-          <v-card-actions>by:{{ question.user.name }}</v-card-actions>
-        </template>
+        <!-- 新規質問投稿ダイアログ -->
+        <v-dialog v-model="dialog">
+      <v-card>
+        <v-card-title>
+          Dialog Title
+        </v-card-title>
+        <v-card-text>
+          <subject-question-form @submitSubjectQuestion="submitSubjectQuestion" @closeDialog="dialog = false"></subject-question-form>
+        </v-card-text>
       </v-card>
-    </v-flex>
+    </v-dialog>
 
 
+    <all-subject-questions :subjectQuestions="subjectQuestions"></all-subject-questions>
 
+    <br>
+    <v-snackbar v-model="snackbar" :timeout="3000" :color="snackbarColor">{{ flashMessage }}</v-snackbar>
   </div>
 </template>
 
 <script>
 
+import AllSubjectQuestions from '../../components/subjectQuestions/AllSubjectQuestions.vue'
+import SubjectQuestionForm from '../../components/subjectQuestions/SubjectQuestionForm.vue'
 import axios from "@/plugins/axios"
 
 export default {
+  components: { AllSubjectQuestions, SubjectQuestionForm },
   async asyncData() {
     try {
-      const responce = await axios.get("/questions/all_questions")
+      const responce = await axios.get("/subject_questions")
       console.log(responce.data)
       return {
-        questions: responce.data
+        subjectQuestions: responce.data
       }
     } catch(error) {
+      console.log(error)
+      throw error
+    }
+  },
+  data() {
+    return {
+      dialog: false,
+      snackbar: false,
+      snackbarColor: "primary",
+      flashMessage: "テストメッセージ",
 
     }
+  },
+  computed: {
+    currentUser() {
+      return this.$store.getters["auth/getCurrentUser"]
+    },
+  },
+  mounted() {
+    if (this.$route.query.message) {
+      this.snackbarColor = "primary"
+      this.snackbar = true
+      this.flashMessage = this.$route.query.message
+      // this.$snackbar.show(this.$route.query.message)
+    }
+  },
+  methods: {
+    async submitSubjectQuestion(data) {
+      try {
+        const response = await axios.post("/subject_questions", {
+            user_id: this.currentUser.id,
+            title: data.title,
+            content: data.content,
+            subject: data.subject
+          }
+        )
+        console.log(response)
+        this.snackbarColor = "primary"
+        this.snackbar = true
+        this.flashMessage = "質問の投稿が完了しました"
+        this.$router.push({ path: `/subjectQuestions/${response.data.id}`, query: { message: '質問の投稿が完了しました' } })
+      } catch(error) {
+        console.log(error)
+        this.snackbarColor = "red accent-2"
+        this.snackbar = true
+        this.flashMessage = "質問を投稿できませんでした"
+      }
+      this.dialog = false
+    },
+
   }
 }
 </script>
