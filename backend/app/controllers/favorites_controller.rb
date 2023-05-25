22c3caa1a43f714ac63125835f12c7e2c1349f78
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-05-26 01:54:28 +0900

    feat:お気に入り一覧ページに科目別質問の一覧を追加

--- backend/app/controllers/favorites_controller.rb
+++ backend/app/controllers/favorites_controller.rb
@@ -6,12 +6,16 @@ class FavoritesController < ApplicationController
     favorite_reviews = current_user.fav_reviews.includes(:user, :book)
     favorite_questions = current_user.fav_questions.includes(:user, :book)
     favorite_replies = current_user.fav_replies.includes(:user, :question)
+    favorite_subject_questions = current_user.fav_subject_questions.includes(:user)
+    favorite_subject_question_replies = current_user.fav_subject_question_replies.includes(:user, :subject_question)
     if current_user
       render json: {
         favorite_books: favorite_books,
         favorite_reviews: favorite_reviews.as_json(include: [:user, :book]),
         favorite_questions: favorite_questions.as_json(include: [:user, :book]),
-        favorite_replies: favorite_replies.as_json(include: [:user, :question])
+        favorite_replies: favorite_replies.as_json(include: [:user, :question]),
+        favorite_subject_questions: favorite_subject_questions.as_json(include: :user),
+        favorite_subject_question_replies: favorite_subject_question_replies.as_json(include: [:user, :subject_question])
       },
       status: 200
     else
