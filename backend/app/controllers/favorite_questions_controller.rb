ff9d5951e3edf69f0081f733950971c20144e084
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-05-31 23:45:36 +0900

    feat:いいね機能の通知機能を作成

--- backend/app/controllers/favorite_questions_controller.rb
+++ backend/app/controllers/favorite_questions_controller.rb
@@ -6,6 +6,7 @@ class FavoriteQuestionsController < ApplicationController
     favorite_question = current_user.favorite_questions.build(question_id: question.id)
     if !exist_favorite_question?(current_user, question)
       favorite_question.save
+      create_notification_favorite_question(current_user, question)
       render json: favorite_question, status: 200
     else
       render json: { error: "エラーが発生しました" }, status: 400
@@ -17,6 +18,7 @@ class FavoriteQuestionsController < ApplicationController
     question = Question.find_by(id: params[:question_id])
     favorite_question = FavoriteQuestion.find_by(user_id: current_user.id, question_id: question.id)
     if favorite_question.destroy
+      delete_notification_favorite_question(current_user, question)
       render json: { status: :ok }
     else
       render json: { error: "エラーが発生しました" }, status: 400
@@ -30,6 +32,24 @@ class FavoriteQuestionsController < ApplicationController
       current_user.fav_questions.include?(question)
     end
 
+    def create_notification_favorite_question(current_user, question)
+      favorite_question_notification = Notification.find_by(action_user_id: current_user.id, question_id: question.id, action_type: "Favorite")
+      if favorite_question_notification.blank?
+        notification = current_user.sent_notifications.build(
+          target_user_id: question.user_id,
+          question_id: question.id,
+          action_type: "Favorite",
+          action_to: "Question",
+        )
+        notification.save if notification.valid?
+      end
+    end
 
+    def delete_notification_favorite_question(current_user, question)
+      favorite_question_notification = Notification.find_by(action_user_id: current_user.id, question_id: question.id, action_type: "Favorite")
+      if favorite_question_notification
+        favorite_question_notification.destroy
+      end
+    end
 
 end
