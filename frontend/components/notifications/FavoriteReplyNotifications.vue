7f32ae39f3c1ef0f26bb978d93cdbc0d179ff326
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-06-05 19:15:32 +0900

    commit

---
+++ frontend/components/notifications/FavoriteReplyNotifications.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="10">
+        <v-card>
+          <v-row>
+            <v-col cols="12">
+              <p>
+                <nuxt-link :to="`/users/${notification.action_user.id}`" class="link-text">{{ notification.action_user.name }}</nuxt-link>さんが
+                <nuxt-link :to="`/books/${notification.reply.question.book_id}/questions/${notification.reply.question_id}/replies/${notification.reply.id}`">あなたの返信</nuxt-link>にいいね!しました
+              </p>
+              <v-card-title>通知の種類: {{ notification.action_type }}</v-card-title>
+              <v-card-text>
+                <p>to: {{ notification.action_to }}</p>
+                <p>これはFavoriteに対する通知</p>
+                <p>これはReplyに対するFavorite</p>
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notification: {}
+  }
+}
+</script>
+
+<style>
+
+</style>
