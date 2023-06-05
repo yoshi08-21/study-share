b35348036a53eb88e0405e2efbc29f91cc54174f
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-06-06 02:13:06 +0900

    feat:参考書の検索結果ページで再検索した際に、検索結果が更新される機能を実装

--- frontend/pages/books/searchBooksResult.vue
+++ frontend/pages/books/searchBooksResult.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <h2>検索結果</h2>
+    <h2>「{{ this.searchBooksKeyword }}」の検索結果</h2>
+
+    <div v-if="searchBooksResult.length > 0">
+      <div v-for="book in searchBooksResult" :key="book.id">
+        <p>{{ book.name }}</p>
+      </div>
+    </div>
+    <div v-else>
+      <p>検索結果はありません</p>
+    </div>
   </div>
 </template>
 
@@ -17,18 +26,47 @@ export default {
         }
       })
       console.log(response.data)
+      return {
+        searchBooksResult: response.data,
+        searchBooksKeyword: route.query.searchBooksKeyword
+      }
     } catch(error) {
       console.log(error)
       throw error
     }
   },
+  watch: {
+    "$route.query.searchBooksKeyword"(newValue, oldValue) {
+      if(newValue !== oldValue) {
+        this.updateSearchResult(newValue)
+          .then(results => {
+            this.searchBooksResult = results
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+    }
+  },
   data() {
     return {
-      searchBooksKeyword: ""
     }
   },
   methods: {
-
+    async updateSearchResult(searchBooksKeyword) {
+      try {
+        const response = await axios.get("/books/search_books", {
+          params: {
+            searchBooksKeyword
+          }
+        })
+        console.log(response.data)
+        return response.data
+      } catch(error) {
+        console.log(error)
+        throw error
+      }
+    },
   }
 }
 </script>
