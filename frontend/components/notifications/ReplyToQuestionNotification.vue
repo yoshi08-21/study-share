28c68a75cc2ced16a3c9882b90a6316750ee1267
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-06-05 19:39:06 +0900

    refactor:通知ごとのメッセージをコンポーネントに置き換え

---
+++ frontend/components/notifications/ReplyToQuestionNotification.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="10">
+        <v-card>
+          <v-row>
+            <v-col cols="12">
+              <p>
+                <nuxt-link :to="`/users/${notification.action_user.id}`" class="link-text">{{ notification.action_user.name }}</nuxt-link>さんが
+                <nuxt-link :to="`/books/${notification.reply.question.book_id}/questions/${notification.reply.question_id}`">あなたの質問</nuxt-link>に
+                <nuxt-link :to="`/books/${notification.reply.question.book_id}/questions/${notification.reply.question_id}/replies/${notification.reply.id}`">返信</nuxt-link>しました
+              </p>
+              <p>
+                →「{{ notification.reply.content }}」
+              </p>
+              <v-card-title>通知の種類: {{ notification.action_type }}</v-card-title>
+              <v-card-text>
+                <p>to: {{ notification.action_to }}</p>
+                <p>これはReplyに対する通知</p>
+                <p>これはReplyに対するReplyNotification</p>
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notification: {}
+  }
+}
+</script>
+
+<style>
+
+</style>
