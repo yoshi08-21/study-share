9e3f035a7c9f06e9e5130820d6da4bf502bb5127
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-06-11 06:14:55 +0900

    feat:科目選択の形式を変更・選択できる科目の数を拡大

--- frontend/components/books/BookForm.vue
+++ frontend/components/books/BookForm.vue
@@ -6,14 +6,9 @@
         cols="12"
         sm="6"
       >
-        <v-select
-          v-model="subject"
-          :items="items"
-          label="科目（必須）"
-          dense
-          outlined
-        ></v-select>
-      </v-col>
+      <v-btn @click="selectSubjectDialog = true">科目を選択する</v-btn>
+    </v-col>
+    <h3 v-if="subject">選択された科目：{{ subject }}</h3>
       <v-text-field counter label="タイトル（必須）" :rules="titleRules" v-model="name"></v-text-field>
       <v-text-field counter label="著者（必須）" :rules="authorRules" v-model="author"></v-text-field>
       <v-text-field counter label="出版社" :rules="publisherRules" v-model="publisher"></v-text-field>
@@ -26,6 +21,27 @@
         </v-col>
       </v-row>
     </v-form>
+
+    <!-- 科目選択ダイアログ -->
+    <v-dialog v-model="selectSubjectDialog" max-width="500px">
+      <v-card>
+        <v-card-title class="headline">科目選択</v-card-title>
+        <v-card-text>
+          <v-radio-group v-model="subject">
+            <template v-for="category in categories">
+              <p :key="category.name">{{ category.name }}</p>
+              <v-radio v-for="subcategory in category.subcategories" :key="subcategory" :label="subcategory" :value="subcategory"></v-radio>
+            </template>
+          </v-radio-group>
+        </v-card-text>
+        <h3>選択された科目:{{ subject }}</h3>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="selectSubjectDialog = false" color="primary" block>閉じる</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </div>
 </template>
 
@@ -42,6 +58,7 @@ export default {
       author: "",
       publisher: "",
       subject: "",
+      keepedSubject: "",
       titleRules: [
         value => !!value || "タイトルを入力してください",
         value => (value || '').length <= 60 || "最大入力文字数は60文字です",
@@ -53,7 +70,35 @@ export default {
       publisherRules: [
         value => (value || '').length <= 25 || "最大入力文字数は25文字です",
       ],
-      items: ["英語", "数学", "国語", "社会", "理科" ]
+      selectSubjectDialog: false,
+      categories: [
+        {
+          name: "国語",
+          subcategories: ["現代文", "古文", "漢文"]
+        },
+        {
+          name: "社会",
+          subcategories: ["日本史", "世界史", "地理", "倫理・政治経済"]
+        },
+        {
+          name: "数学",
+          subcategories: ["数学I・A", "数学Ⅱ・B", "数学Ⅲ・C"]
+        },
+        {
+          name: "英語",
+          subcategories: ["英文法", "英文読解", "英作文", "英単語", "リスニング"]
+        },
+        {
+          name: "理科",
+          subcategories: ["物理", "生物", "化学", "地学"]
+        },
+        {
+          name: "その他",
+          subcategories: ["過去問", "小論文", "その他"]
+        },
+      ],
+
+
     }
   },
   methods: {
