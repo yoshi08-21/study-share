3550bb91376f9fb8098a8bf382c727307f0d379b
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-08-07 23:33:40 +0900

    add:Active Storageを導入

---
+++ backend/config/initializers/aws.rb
@@ -0,0 +1,7 @@
+access = Rails.application.credentials.aws[:access_key_id]
+secret = Rails.application.credentials.aws[:secret_access_key]
+
+Aws.config.update({
+  region: 'ap-northeast-1',
+  credentials: Aws::Credentials.new(access, secret),
+})
