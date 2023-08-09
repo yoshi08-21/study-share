6b3972bcdb661c62cbabc149a127ee4e57f97c60
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-08-09 17:58:06 +0900

    feat:画像投稿の際に、ファイルの形式やファイルサイズを検証する機能を追加

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -10,12 +10,31 @@ class Question < ApplicationRecord
 
   has_one_attached :image
 
-
-
   validates :title,    presence: true, length: { maximum: 60 }
   validates :content,  presence: true, length: { maximum: 1000 }
   validates :subject,  presence: true
   validates :user_id,    presence: true
   validates :book_id,    presence: true
+  validate :image_size_validation
+  validate :acceptable_image
+
+
+
+  private
+
+  def image_size_validation
+    if image.attached? && image.blob.byte_size > 3.megabytes
+      errors.add(:image, "は3MB以下のファイルにしてください")
+    end
+  end
+
+  def acceptable_image
+    return unless image.attached?
+
+    unless image.blob.content_type.start_with?('image/jpeg', 'image/png')
+      errors.add(:image, 'はjpegまたはpng形式である必要があります')
+    end
+  end
+
 
 end
