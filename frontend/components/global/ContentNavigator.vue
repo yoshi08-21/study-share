7270e885f5003062a8b8035e4c1b5f36053b80c9
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-08-17 19:57:32 +0900

    style:質問詳細ページのスタイルを調整・詳細ページのお気に入りボタン・前後のコンテンツへの移動ボタンをコンポーネントに置き換え

---
+++ frontend/components/global/ContentNavigator.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row class="d-flex justify-space-between">
+    <v-col cols="6" class="d-flex justify-start">
+      <v-btn @click="previousContent" x-large><v-icon>mdi-arrow-left-bold</v-icon>前の{{ content }}</v-btn>
+    </v-col>
+    <v-col cols="6" class="d-flex justify-end">
+      <v-btn @click="nextContent" x-large>次の{{ content }}<v-icon>mdi-arrow-right-bold</v-icon></v-btn>
+    </v-col>
+  </v-row>
+
+</template>
+
+<script>
+export default {
+  props: {
+    content: String,
+  },
+  methods: {
+    previousContent() {
+      this.$emit("previousContent")
+    },
+    nextContent() {
+      this.$emit("nextContent")
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
