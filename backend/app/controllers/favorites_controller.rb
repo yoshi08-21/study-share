97e5c144977d1dad8d8b8397e893ff8edf278320
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-08-17 18:54:19 +0900

    style:質問詳細ページのスタイルを変更

--- backend/app/controllers/favorites_controller.rb
+++ backend/app/controllers/favorites_controller.rb
@@ -7,15 +7,15 @@ class FavoritesController < ApplicationController
     favorite_books = current_user.fav_books.with_attached_image.select("books.*, (SELECT COUNT(*) FROM reviews WHERE reviews.book_id = books.id) AS reviews_count, (SELECT ROUND(AVG(reviews.rating), 1) FROM reviews where reviews.book_id = books.id) AS average_rating, (SELECT COUNT(*) FROM favorite_books WHERE favorite_books.book_id = books.id) AS favorite_books_count, (SELECT COUNT(*) FROM favorite_books WHERE favorite_books.book_id = books.id and favorite_books.user_id = #{current_user.id}) AS check_favorite")
     favorite_books_with_images = attach_image_to_books(favorite_books)
 
-    favorite_reviews = current_user.fav_reviews.includes(:user, :book).select("reviews.*, (SELECT COUNT(*) FROM favorite_reviews WHERE favorite_reviews.review_id = reviews.id) AS favorite_reviews_count")
+    favorite_reviews = current_user.fav_reviews.includes(book: { image_attachment: :blob }, user: { image_attachment: :blob }).select("reviews.*, (SELECT COUNT(*) FROM favorite_reviews WHERE favorite_reviews.review_id = reviews.id) AS favorite_reviews_count")
     favorite_reviews_with_images = attach_image_to_reviews(favorite_reviews)
 
-    favorite_questions = current_user.fav_questions.includes(:user, :book).select("questions.*, (SELECT COUNT(*) FROM replies WHERE replies.question_id = questions.id) AS replies_count, (SELECT COUNT(*) FROM favorite_questions WHERE favorite_questions.question_id = questions.id) AS favorite_questions_count")
+    favorite_questions = current_user.fav_questions.includes(book: { image_attachment: :blob }, user: { image_attachment: :blob }).select("questions.*, (SELECT COUNT(*) FROM replies WHERE replies.question_id = questions.id) AS replies_count, (SELECT COUNT(*) FROM favorite_questions WHERE favorite_questions.question_id = questions.id) AS favorite_questions_count")
     favorite_questions_with_images = attach_image_to_questions(favorite_questions)
 
     favorite_replies = current_user.fav_replies.includes(:user, :question)
 
-    favorite_subject_questions = current_user.fav_subject_questions.includes(:user).select("subject_questions.*, (SELECT COUNT(*) FROM subject_question_replies WHERE subject_question_replies.subject_question_id = subject_questions.id) AS subject_question_replies_count, (SELECT COUNT(*) FROM favorite_subject_questions WHERE favorite_subject_questions.subject_question_id = subject_questions.id) AS favorite_subject_questions_count")
+    favorite_subject_questions = current_user.fav_subject_questions.includes(user: { image_attachment: :blob }).select("subject_questions.*, (SELECT COUNT(*) FROM subject_question_replies WHERE subject_question_replies.subject_question_id = subject_questions.id) AS subject_question_replies_count, (SELECT COUNT(*) FROM favorite_subject_questions WHERE favorite_subject_questions.subject_question_id = subject_questions.id) AS favorite_subject_questions_count")
     favorite_subject_questions_with_images = attach_image_to_subject_questions(favorite_subject_questions)
 
     favorite_subject_question_replies = current_user.fav_subject_question_replies.includes(:user, :subject_question)
