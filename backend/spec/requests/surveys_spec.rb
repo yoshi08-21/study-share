f0691efc39de4c1dc30ceb81ea782e9d231ea518
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-08-25 04:30:45 +0900

    add:アンケートの回答機能に関するテストを記述

--- backend/spec/requests/surveys_spec.rb
+++ backend/spec/requests/surveys_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "Surveys", type: :request do
 
   let(:user) { create(:user) }
-  let(:user2) { create(:user) }
   let(:survey) { create(:survey, user_id: user.id) }
 
   describe "アンケートの一覧表示" do
