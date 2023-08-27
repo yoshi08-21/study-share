0206f70302e6ce9581d1f4002d001066ee042a9d
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-08-28 00:27:14 +0900

    refactor:surveysコントローラーと関連するテストをリファクタリング

--- backend/spec/requests/surveys_spec.rb
+++ backend/spec/requests/surveys_spec.rb
@@ -6,18 +6,57 @@ RSpec.describe "Surveys", type: :request do
   let(:survey) { create(:survey, user_id: user.id) }
 
   describe "アンケートの一覧表示" do
+
     context "アンケートが3件作成されている状態でアンケート一覧ページに遷移すると" do
+
       it "3件のアンケートが表示される" do
-        survey
+        user = create(:user, name: "アンケート作成者")
+        create(:survey, title: "テストタイトル", content: "テスト本文", genre: "現代文", status: false, user_id: user.id)
         create(:survey, user_id: user.id)
         create(:survey, user_id: user.id)
 
         get surveys_path
         expect(response).to have_http_status(200)
+
+        json_response = JSON.parse(response.body)
+        aggregate_failures do
+          expect(json_response.length).to eq(3)
+          expect(json_response[0]["title"]).to eq("テストタイトル")
+          expect(json_response[0]["content"]).to eq("テスト本文")
+          expect(json_response[0]["genre"]).to eq("現代文")
+          expect(json_response[0]["status"]).to eq(false)
+          expect(json_response[0]["user"]["name"]).to eq("アンケート作成者")
+        end
+
         expect(JSON.parse(response.body).length).to eq(3)
       end
     end
-    context "アンケートが投稿されていない状態でアンケート一覧ページに遷移すると" do
+
+    context "アンケートにいいねがされていると" do
+      it "いいねの件数が表示される" do
+        user2 = create(:user)
+        create(:favorite_survey, user_id: user.id, survey_id: survey.id)
+        create(:favorite_survey, user_id: user2.id, survey_id: survey.id)
+
+        get surveys_path
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)[0]["favorite_surveys_count"]).to eq(2)
+      end
+    end
+
+    context "アンケートに回答が投稿されていると" do
+      it "回答の件数が表示される" do
+        user2 = create(:user)
+        create(:survey_answer, user_id: user.id, survey_id: survey.id)
+        create(:survey_answer, user_id: user2.id, survey_id: survey.id)
+
+        get surveys_path
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)[0]["survey_answers_count"]).to eq(2)
+      end
+    end
+
+    context "アンケートが投稿されていないと" do
       it "空の配列が返ってくる" do
         get surveys_path
         expect(response).to have_http_status(200)
@@ -25,8 +64,11 @@ RSpec.describe "Surveys", type: :request do
         expect(JSON.parse(response.body)).to eq([])
       end
     end
+
   end
+
   describe "アンケートの詳細表示" do
+
     context "存在するアンケートの詳細ページに遷移すると" do
       it "アンケートの詳細情報が取得できる" do
         image_path = Rails.root.join("spec", "fixtures", "files", "no_image.png")
@@ -34,9 +76,7 @@ RSpec.describe "Surveys", type: :request do
         user = create(:user, name: "アンケート作成者")
         survey = create(:survey, title: "テストタイトル", content: "テスト本文", option1: "選択肢1", option2: "選択肢2", option3: "選択肢3", option4: "選択肢4", genre: "英語", image: image, user_id: user.id)
 
-        get survey_path(survey), params: {
-          user_id: user.id
-        }
+        get survey_path(survey)
         expect(response).to have_http_status(200)
 
         json_response = JSON.parse(response.body)
@@ -54,7 +94,32 @@ RSpec.describe "Surveys", type: :request do
         end
       end
     end
-    context "存在しないアンケートの詳細ページに遷移すると" do
+
+    context "アンケートにいいねがされていると" do
+      it "いいねの件数が表示される" do
+        user2 = create(:user)
+        create(:favorite_survey, user_id: user.id, survey_id: survey.id)
+        create(:favorite_survey, user_id: user2.id, survey_id: survey.id)
+
+        get survey_path(survey)
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)["survey"]["favorite_surveys_count"]).to eq(2)
+      end
+    end
+
+    context "アンケートに回答が投稿されていると" do
+      it "回答の件数が表示される" do
+        user2 = create(:user)
+        create(:survey_answer, user_id: user.id, survey_id: survey.id)
+        create(:survey_answer, user_id: user2.id, survey_id: survey.id)
+
+        get survey_path(survey)
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)["survey"]["survey_answers_count"]).to eq(2)
+      end
+    end
+
+    context "アンケートが存在しないと" do
       it "アンケートの取得に失敗する" do
         get survey_path(-1), params: {
           user_id: user.id
@@ -62,8 +127,11 @@ RSpec.describe "Surveys", type: :request do
         expect(response).to have_http_status(404)
       end
     end
+
   end
+
   describe "アンケートの新規作成" do
+
     context "正しいパラメーターでアンケートを作成すると" do
       it "アンケートの作成に成功する" do
         image_path = Rails.root.join("spec", "fixtures", "files", "no_image.png")
@@ -104,6 +172,7 @@ RSpec.describe "Surveys", type: :request do
         expect(JSON.parse(response.body)["survey"]["image"]).to include("no_image.png")
       end
     end
+
     context "不正なパラメーターでアンケートを作成すると" do
       it "アンケートの作成に失敗する" do
         expect {
@@ -119,12 +188,35 @@ RSpec.describe "Surveys", type: :request do
             }
           }
         }.to change(Survey, :count).by(0)
-        expect(response).to have_http_status(400)
-        expect(JSON.parse(response.body)["error"]).to eq("エラーが発生しました")
+        expect(response).to have_http_status(422)
       end
     end
+
+    context "ログインしていない(current_userが存在しない)と" do
+      it "アンケートの作成に失敗する" do
+        expect {
+          post surveys_path, params: {
+            survey: {
+              title: "テストアンケートタイトル",
+              content: "テストアンケート本文",
+              option1: "選択肢1",
+              option2: "選択肢2",
+              option3: "選択肢3",
+              option4: "選択肢4",
+              status: 0,
+              genre: "数学",
+              user_id: -1
+            }
+          }
+        }.to change(Survey, :count).by(0)
+        expect(response).to have_http_status(404)
+      end
+    end
+
   end
+
   describe "アンケートの削除機能" do
+
     context "自分が作成したアンケートを削除すると" do
       it "アンケートの削除に成功する" do
         user = create(:user, name: "アンケート作成者")
@@ -138,6 +230,7 @@ RSpec.describe "Surveys", type: :request do
         expect(response).to have_http_status(204)
       end
     end
+
     context "他人が作成したアンケートを削除すると" do
       it "アンケートの削除に失敗する" do
         user = create(:user, name: "アンケート作成者")
@@ -149,12 +242,38 @@ RSpec.describe "Surveys", type: :request do
             user_id: user2.id
           }
         }.to change(Survey, :count).by(0)
-        expect(response).to have_http_status(400)
+        expect(response).to have_http_status(422)
         expect(JSON.parse(response.body)["error"]).to eq("権限がありません")
       end
     end
+
+    context "ログインしていない(current_userが存在しない)と" do
+      it "アンケートの削除に失敗する" do
+      survey = create(:survey, user_id: user.id)
+        expect {
+          delete survey_path(survey), params: {
+            user_id: -1
+          }
+        }.to change(Survey, :count).by(0)
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    context "アンケートが存在しないと" do
+      it "アンケートの作成に失敗する" do
+        expect {
+          delete survey_path(-1), params: {
+            user_id: user.id
+          }
+        }.to change(Survey, :count).by(0)
+        expect(response).to have_http_status(404)
+      end
+    end
+
   end
+
   describe "アンケートの締め切り機能" do
+
     context "自分が作成したアンケートを締め切ると" do
       it "アンケートの締切に成功する" do
         user = create(:user, name: "アンケート作成者")
@@ -171,6 +290,7 @@ RSpec.describe "Surveys", type: :request do
         expect(JSON.parse(response.body)["survey"]["status"]).to eq(true)
       end
     end
+
     context "他人が作成したアンケートを締め切ると" do
       it "アンケートの締切に失敗する" do
         user = create(:user, name: "アンケート作成者")
@@ -180,12 +300,52 @@ RSpec.describe "Surveys", type: :request do
         patch close_survey_survey_path(survey), params: {
           user_id: user2.id
         }
-        expect(response).to have_http_status(400)
+        expect(response).to have_http_status(422)
         expect(JSON.parse(response.body)["error"]).to eq("権限がありません")
       end
     end
+
+    context "ログインしていない(current_userが存在しない)と" do
+      it "アンケートの締切に失敗する" do
+        user = create(:user, name: "アンケート作成者")
+        survey = create(:survey, title: "テストタイトル", content: "テスト本文", option1: "選択肢1", option2: "選択肢2", status: 0, genre: "英語", user_id: user.id)
+
+        patch close_survey_survey_path(survey), params: {
+          user_id: -1
+        }
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    context "アンケートが存在しないと" do
+      it "アンケートの締切に失敗する" do
+        user = create(:user, name: "アンケート作成者")
+        survey = create(:survey, title: "テストタイトル", content: "テスト本文", option1: "選択肢1", option2: "選択肢2", status: 0, genre: "英語", user_id: user.id)
+
+        patch close_survey_survey_path(-1), params: {
+          user_id: user.id
+        }
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    context "アンケートがすでに締め切られていると" do
+      it "アンケートの締切に失敗する" do
+        user = create(:user, name: "アンケート作成者")
+        survey = create(:survey, title: "テストタイトル", content: "テスト本文", option1: "選択肢1", option2: "選択肢2", status: true, genre: "英語", user_id: user.id)
+
+        patch close_survey_survey_path(survey), params: {
+          user_id: user.id
+        }
+        expect(response).to have_http_status(422)
+        expect(JSON.parse(response.body)["error"]).to eq("アンケートはすでに締め切られています")
+      end
+    end
+
   end
+
   describe "アンケート情報の取得" do
+
     context "check_existenceで存在するアンケートをチェックすると" do
       it "成功する" do
         get check_existence_surveys_path, params: {
