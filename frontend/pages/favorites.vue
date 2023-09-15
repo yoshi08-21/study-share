3eea5aa30fe1380152727fd78dfc7f75e1dd9dc2
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-09-16 07:54:51 +0900

    feat:ユーザー詳細ページの投稿一覧・お気に入り一覧にページネーションを追加

--- frontend/pages/favorites.vue
+++ frontend/pages/favorites.vue
@@ -4,45 +4,74 @@
 
     <br>
     <v-tabs v-model="tab">
-      <v-tab v-for="(tabItem, index) in tabs" :key="index">
+      <v-tab v-for="(tabItem, index) in tabs" :key="index" @click="page = 1">
         {{ tabItem.label }}
       </v-tab>
     </v-tabs>
 
     <template v-if="tab === 0">
       <br><br><br>
-      <each-books :books="favoriteBooks"></each-books>
+      <v-pagination v-model="page" :length="booksTotalPages"></v-pagination>
+      <br>
+      <each-books :books="booksChunk"></each-books>
+      <br>
+      <v-pagination v-model="page" :length="booksTotalPages"></v-pagination>
     </template>
 
     <template v-else-if="tab === 1">
       <br><br><br>
-      <each-reviews :reviews="favoriteReviews"></each-reviews>
+      <v-pagination v-model="page" :length="reviewsTotalPages"></v-pagination>
+      <br>
+      <each-reviews :reviews="reviewsChunk"></each-reviews>
+      <br>
+      <v-pagination v-model="page" :length="reviewsTotalPages"></v-pagination>
     </template>
 
     <template v-else-if="tab === 2">
       <br><br><br>
-      <each-questions :questions="favoriteQuestions"></each-questions>
+      <v-pagination v-model="page" :length="questionsTotalPages"></v-pagination>
+      <br>
+      <each-questions :questions="questionsChunk"></each-questions>
+      <br>
+      <v-pagination v-model="page" :length="questionsTotalPages"></v-pagination>
     </template>
 
     <template v-else-if="tab === 3">
       <br><br><br>
-      <each-subject-questions :subjectQuestions="favoriteSubjectQuestions"></each-subject-questions>
+      <v-pagination v-model="page" :length="subjectQuestionsTotalPages"></v-pagination>
+      <br>
+      <each-subject-questions :subjectQuestions="subjectQuestionsChunk"></each-subject-questions>
+      <br>
+      <v-pagination v-model="page" :length="subjectQuestionsTotalPages"></v-pagination>
     </template>
 
     <template v-else-if="tab === 4">
       <br><br><br>
-      <each-replies :replies="favoriteReplies"></each-replies>
+      <v-pagination v-model="page" :length="repliesTotalPages"></v-pagination>
+      <br>
+      <each-replies :replies="repliesChunk"></each-replies>
+      <br>
+      <v-pagination v-model="page" :length="repliesTotalPages"></v-pagination>
+
     </template>
 
     <template v-else-if="tab === 5">
       <br><br><br>
-      <each-subject-question-replies :subjectQuestionReplies="favoriteSubjectQuestionReplies"></each-subject-question-replies>
+      <v-pagination v-model="page" :length="subjectQuestionRepliesTotalPages"></v-pagination>
+      <br>
+      <each-subject-question-replies :subjectQuestionReplies="subjectQuestionRepliesChunk"></each-subject-question-replies>
+      <br>
+      <v-pagination v-model="page" :length="subjectQuestionRepliesTotalPages"></v-pagination>
+
     </template>
 
     <template v-else-if="tab === 6">
       <br><br><br>
-      <each-surveys :surveys="favoriteSurveys"></each-surveys>
-
+      <v-pagination v-model="page" :length="surveysTotalPages"></v-pagination>
+      <br>
+      <each-surveys :surveys="surveysChunk"></each-surveys>
+      <br>
+      <v-pagination v-model="page" :length="surveysTotalPages"></v-pagination>
     </template>
 
 
@@ -57,8 +86,8 @@ import EachQuestions from '../components/questions/EachQuestions.vue'
 import EachReplies from '../components/replies/EachReplies.vue'
 import EachSubjectQuestions from '../components/subjectQuestions/EachSubjectQuestions.vue'
 import EachSubjectQuestionReplies from '../components/subjectQuestionReplies/EachSubjectQuestionReplies.vue'
-import authCheck from '../middleware/authCheck'
 import EachSurveys from '../components/surveys/EachSurveys.vue'
+import authCheck from '../middleware/authCheck'
 import axios from "@/plugins/axios"
 
 export default {
@@ -97,11 +126,70 @@ export default {
         { label: "いいね!した返信" },
         { label: "いいね!した返信(科目別質問)" },
         { label: "いいね!したアンケート" },
-
-      ]
-
+      ],
+      perPage: 10,
+      page: 1,
     }
-  }
+  },
+  computed: {
+    booksChunk() {
+      const start = (this.page - 1) * this.perPage
+      const end = start + this.perPage
+      return this.favoriteBooks.slice(start, end)
+    },
+    reviewsChunk() {
+      const start = (this.page - 1) * this.perPage
+      const end = start + this.perPage
+      return this.favoriteReviews.slice(start, end)
+    },
+    questionsChunk() {
+      const start = (this.page - 1) * this.perPage
+      const end = start + this.perPage
+      return this.favoriteQuestions.slice(start, end)
+    },
+    repliesChunk() {
+      const start = (this.page - 1) * this.perPage
+      const end = start + this.perPage
+      return this.favoriteReplies.slice(start, end)
+    },
+    subjectQuestionsChunk() {
+      const start = (this.page - 1) * this.perPage
+      const end = start + this.perPage
+      return this.favoriteSubjectQuestions.slice(start, end)
+    },
+    subjectQuestionRepliesChunk() {
+      const start = (this.page - 1) * this.perPage
+      const end = start + this.perPage
+      return this.favoriteSubjectQuestionReplies.slice(start, end)
+    },
+    surveysChunk() {
+      const start = (this.page - 1) * this.perPage
+      const end = start + this.perPage
+      return this.favoriteSurveys.slice(start, end)
+    },
+    booksTotalPages() {
+      return Math.ceil(this.favoriteBooks.length / this.perPage);
+    },
+    reviewsTotalPages() {
+      return Math.ceil(this.favoriteReviews.length / this.perPage);
+    },
+    questionsTotalPages() {
+      return Math.ceil(this.favoriteQuestions.length / this.perPage);
+    },
+    repliesTotalPages() {
+      return Math.ceil(this.favoriteReplies.length / this.perPage);
+    },
+    subjectQuestionsTotalPages() {
+      return Math.ceil(this.favoriteSubjectQuestions.length / this.perPage);
+    },
+    subjectQuestionRepliesTotalPages() {
+      return Math.ceil(this.favoriteSubjectQuestionReplies.length / this.perPage);
+    },
+    surveysTotalPages() {
+      return Math.ceil(this.favoriteSurveys.length / this.perPage);
+    },
+
+  },
 }
 </script>
 
