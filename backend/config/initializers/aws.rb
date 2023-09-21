5dd36a4baf4710c1917313381edc79255ec05250
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-09-22 06:36:22 +0900

    add:.aws/credentialsに新規ユーザーを追加

--- backend/config/initializers/aws.rb
+++ backend/config/initializers/aws.rb
@@ -5,3 +5,6 @@ Aws.config.update({
   region: 'ap-northeast-1',
   credentials: Aws::Credentials.new(access, secret),
 })
+
+ssm_credentials = Aws::SharedCredentials.new(profile_name: 'ssm_user')
+$ssm_client = Aws::SSM::Client.new(credentials: ssm_credentials)
