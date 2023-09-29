aff27ca328824057b8e552b45e22307df924533b
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-09-30 08:21:51 +0900

    aws.rbを修正

--- backend/config/initializers/aws.rb
+++ backend/config/initializers/aws.rb
@@ -1,8 +1,9 @@
-access = ENV['AWS_ACCESS_KEY_ID'] || Rails.application.credentials.aws[:access_key_id]
-secret = ENV['AWS_SECRET_ACCESS_KEY'] || Rails.application.credentials.aws[:secret_access_key]
+unless Rails.env.test?
+  access = ENV['AWS_ACCESS_KEY_ID'] || Rails.application.credentials.aws[:access_key_id]
+  secret = ENV['AWS_SECRET_ACCESS_KEY'] || Rails.application.credentials.aws[:secret_access_key]
 
-Aws.config.update({
-  region: 'ap-northeast-1',
-  credentials: Aws::Credentials.new(access, secret),
-})
-
+  Aws.config.update({
+    region: 'ap-northeast-1',
+    credentials: Aws::Credentials.new(access, secret),
+  })
+end
