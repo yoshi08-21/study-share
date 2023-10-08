42d8ff3804800d32faa15e34a7dfe3e4e6a7e127
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-10-08 16:34:07 +0900

    style:ユーザー登録時に送信されるメールのタイトルと本文を修正

--- backend/app/mailers/user_mailer.rb
+++ backend/app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
   def registration_email(user, email_verification_number)
     @user = user
     @email_verification_number = email_verification_number
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: "メールアドレスの確認")
   end
 
 end
