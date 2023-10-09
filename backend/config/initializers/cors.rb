6b2556f2c5ad9a53950e04615aedf5dc03298adf
yoshi-t0821@keio.jp <yoshito-t0821@keio.jp>
2023-10-09 16:30:30 +0900

    独自ドメインの導入に伴い、URLの記述を変更

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.production?
-      origins 'http://www.study-share.com.s3-website-ap-northeast-1.amazonaws.com'
+      origins 'https://study-share.com'
     else
       origins 'http://localhost:8080'
     end
